4c9fb9a4914b3e956c4899a16764b8f2c6bfa184
Nicholas Brown <countdooku.steak@gmail.com>
2024-07-29 18:39:38 -0700

    add fileWatcherUtils to elixir

--- modules/elixir.nix
+++ modules/elixir.nix
@@ -1,23 +1,49 @@
 { pkgs, lib, config, ... }:
 let cfg = config.elixir;
-in with lib; {
+in {
   options = {
     elixir = {
-      enabled = mkOption {
-        type = types.bool;
+      enabled = lib.mkOption {
+        type = lib.types.bool;
         default = false;
         description = ''
           Whether to include elixir packages in the development environment.
         '';
       };
+
+      fileWatcherUtils = {
+        enabled = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description =
+            "Whether to include file watcher utils for Phoenix development";
+        };
+      };
     };
   };
 
-  config = {
-    buildInputs = mkIf cfg.enabled (with pkgs; [
-      erlang_26
-      beam.packages.erlang_26.elixir_1_14
+  config = lib.mkIf cfg.enabled {
+    buildInputs = [
+      pkgs.erlang_26
+      pkgs.beam.packages.erlang_26.elixir_1_14
 
-    ]);
+    ] ++ lib.optionals cfg.fileWatcherUtils.enabled
+      (lib.optionals pkgs.stdenv.isLinux [
+        # For ExUnit Notifier on Linux.
+        pkgs.libnotify
+
+        # For file_system on Linux.
+        pkgs.inotify-tools
+
+        # necessary for alpine linux to work properly
+        pkgs.glibcLocales
+      ] ++ lib.optionals pkgs.stdenv.isDarwin [
+        # For ExUnit Notifier on macOS.
+        pkgs.terminal-notifier
+
+        # For file_system on macOS.
+        pkgs.darwin.apple_sdk.frameworks.CoreFoundation
+        pkgs.darwin.apple_sdk.frameworks.CoreServices
+      ]);
   };
 }
